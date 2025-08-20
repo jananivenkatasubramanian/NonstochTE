ab123b0bd440989b2861880e897cf276d2ee7ec4
Janani Venkatasubramanian <55430193+jananivenkatasubramanian@users.noreply.github.com>
2025-08-20 16:09:07 +0200

    Update initialize_nonstoch_guarantees.m

--- initialize_nonstoch_guarantees.m
+++ initialize_nonstoch_guarantees.m
@@ -6,6 +6,9 @@
 % freqs=[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9];
 freqs=[0:floor(T/fsel)*(1/T):1-(1/T)];
 % freqs=[0,0.1,0.2,0.3,0.4,0.5,0.6];
+
+
+% freqs=[0,0.1];
 L=size(freqs,2);
 
 % choose A, B
@@ -17,18 +20,21 @@ L=size(freqs,2);
 % A=[0.49 0.49 0;0 0.49 0.49;0 0 0.49];
 % B=[0; 0; 0.49];
 
+% A=[0.4 0.8 0;0 0.5 0.6;0 0 0.3];
+% B=[0; 0; 0.9];
+
 % 4x4
 % A=[0.49 0.49 0 0;0 0.49 0.49 0; 0 0 0.49 0.49;0 0 0 0.49];
 % B=[0; 0; 0; 0.49];
 
 % Chain of 2 mass-spring-dampers: 4x4
-Ts=0.1; % sampling period
-m1=20; % mass-1
-m2=10; % mass-2
-k1=3; % spring constant 1
-k2=1; % spring constant 2
-d1=0.01; % damping coefficient 1
-d2=0.03; % damping coefficient 2
+Ts=0.5; % sampling period
+m1=1; % mass-1
+m2=2; % mass-2
+k1=1; % spring constant 1
+k2=1.5; % spring constant 2
+d1=0.5; % damping coefficient 1
+d2=1.1; % damping coefficient 2
 A=[1,Ts,0,0;...
   (-Ts*(k1+k2)/m2),(1-(Ts*(d1+d2)/m1)), (Ts*k2/m1), (Ts*d2/m1);...
   0,0,1,Ts;...
@@ -49,4 +55,3 @@ for i=1:T
 end
 
 eps=0.5;
-
