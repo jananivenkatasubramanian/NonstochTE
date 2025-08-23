e06ec66e32c6da4d5e320d384257674c4fdab29a
Janani Venkatasubramanian <55430193+jananivenkatasubramanian@users.noreply.github.com>
2025-08-23 10:26:31 +0200

    Update exploration_scaleddown.m

--- exploration_scaleddown.m
+++ exploration_scaleddown.m
@@ -7,14 +7,16 @@ cvx_solver SDPT3
 
 %% Variables
 
-dtemp=zeros(nx*nphi*nphi,nx*nphi*nphi);
+d0temp=zeros(nx*nphi*nphi,nx*nphi*nphi);
 
 variable D1((nphi)+(nx*nphi*nphi),(nphi)+(nx*nphi*nphi)) symmetric
 variable D1s(1+(nx*nphi),1+(nx*nphi)) symmetric
 variable D2((nphi)+(nx*nphi*nphi),(nphi)+(nx*nphi*nphi)) symmetric
 variable D2s(1+(nx*nphi),1+(nx*nphi)) symmetric
-variable D3((nphi)+(nx*nphi*nphi),(nphi)+(nx*nphi*nphi)) symmetric
-variable D3s(1+(nx*nphi),1+(nx*nphi)) symmetric
+% variable D3((nphi)+(nx*nphi*nphi),(nphi)+(nx*nphi*nphi)) symmetric
+% variable D3s(1+(nx*nphi),1+(nx*nphi)) symmetric
+variable D3s(nphi,nphi) symmetric
+% variable dtemp(nx*nphi*nphi,nx*nphi*nphi) symmetric
 
 variable tau1(1)
 variable tau2(1)
@@ -33,7 +35,9 @@ end
 
 Se_energy=[gammae, ones(1,L)*(Ue'); Ue*ones(L,1), gammae*eye(nu*L)];
 
-Se_D=kron(D1s,eye(nphi))+kron(D2s,eye(nphi))+D3;
+Se_D1=kron(D1s,eye(nphi))+kron(D2s,eye(nphi))+blkdiag(D3s,d0temp);
+Se_D2=kron(D1s,eye(nphi))+kron(D2s,eye(nphi));
+% Se_D=kron(D1s,eye(nphi))+kron(D2s,eye(nphi))+blkdiag(D3s,dtemp);
 
 %% Exploration LMI-1: S_exploration_1
 % Ue=zeros(nu*L,L);
@@ -145,6 +149,34 @@ S2_2=[s22_11, s22_12; s22_21, s22_22];
 S_exploration_2=S2_1-(tau2*S2_2);
 
 %% Exploration LMI-3: S_exploration_3
+% % D311=zeros(nx*nphi);
+% % tau3=0;
+% % dtemp=zeros((nx*nphi)^2,(nx*nphi)^2);
+% 
+% % S_exploration_3 = S3_1 - tau3 * S3_2
+% % S3_1
+% UNCOMMENT AFTER THIS LINE------------------
+% s31_11=(1-eps)*(Ue*Utilde' + Utilde*Ue' - Utilde*Utilde');
+% s31_12=zeros(L*nu,nphi+(nx*nphi*nphi));
+% s31_21=s31_12';
+% s31_22_temp=-((1-eps)/eps)*barWphi - (1/T)*gamma_w*D_des_tilde;
+% s31_22=blkdiag(s31_22_temp,d0temp) - D3;
+% 
+% S3_1=[s31_11, s31_12; s31_21, s31_22];
+ 
+% % S3_2
+% UNCOMMENT AFTER THIS LINE------------------
+% s32_11= -eye(nu*L);
+% s32_12= [Vphi0;zeros(nx*nphi*nphi,L*nu)]';
+% s32_21=s32_12';
+% s32_22=blkdiag(Gamma_phi-(Vphi0*Vphi0'),d0temp);
+% s32_22=(s32_22+s32_22')/2;
+% 
+% S3_2=[s32_11, s32_12; s32_21, s32_22];
+% 
+% S_exploration_3= S3_1-(tau3*S3_2);
+
+%% Exploration LMI-3: S_exploration_3 (SCALE DOWN)
 % D311=zeros(nx*nphi);
 % tau3=0;
 % dtemp=zeros((nx*nphi)^2,(nx*nphi)^2);
@@ -152,18 +184,17 @@ S_exploration_2=S2_1-(tau2*S2_2);
 % S_exploration_3 = S3_1 - tau3 * S3_2
 % S3_1
 s31_11=(1-eps)*(Ue*Utilde' + Utilde*Ue' - Utilde*Utilde');
-s31_12=zeros(L*nu,nphi+(nx*nphi*nphi));
+s31_12=zeros(L*nu,nphi);
 s31_21=s31_12';
-s31_22_temp=-((1-eps)/eps)*barWphi - (1/T)*gamma_w*D_des_tilde;
-s31_22=blkdiag(s31_22_temp,dtemp) - D3;
+s31_22=-((1-eps)/eps)*barWphi - (1/T)*gamma_w*D_des_tilde - D3s;
 
 S3_1=[s31_11, s31_12; s31_21, s31_22];
 
 % S3_2
 s32_11= -eye(nu*L);
-s32_12= [Vphi0;zeros(nx*nphi*nphi,L*nu)]';
+s32_12= [Vphi0]';
 s32_21=s32_12';
-s32_22=blkdiag(Gamma_phi-(Vphi0*Vphi0'),dtemp);
+s32_22=Gamma_phi-(Vphi0*Vphi0');
 s32_22=(s32_22+s32_22')/2;
 
 S3_2=[s32_11, s32_12; s32_21, s32_22];
@@ -175,7 +206,8 @@ S_exploration_3= S3_1-(tau3*S3_2);
 minimize gammae
 
 Se_energy>=0;
-Se_D>=0;
+Se_D1>=0;
+Se_D2>=0;
 % D1+D2>=0;
 tau1>=0;
 tau2>=0;
